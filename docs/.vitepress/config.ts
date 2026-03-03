c807d5f99087ef173b342e49f1ea762581512032
imsunxinhao <imsunxinhao@163.com>
2026-03-03 09:47:47 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,13 +6,38 @@ export default defineConfig({
   description: '使用 Hydro 构建的稳定，插件开源的 Online-Judge 系统。',
   
   themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '插件开源', link: '/opensource' },
+      { text: '关于', link: '/about-uranusoj' }
+    ],
+
     sidebar: [
       {
-        text: '导航',
+        text: '入门指南',
         items: [
-          { text: '关于 UranusOJ', link: '/about-uranusoj' }
+          { text: '关于 UranusOJ', link: '/about-uranusoj' },
+          { text: '插件开源', link: '/opensource' },
+          { text: '安装部署', link: '/installation' }
         ]
-      }
-    ]
-  }
+      },
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/UranusOJ/docs' }
+    ],
+    footer: {
+      message: '基于 LGPL 3.0 协议开源',
+      copyright: 'Copyright © 2026 UranusOJ'
+    },
+    editLink: {
+      pattern: 'https://github.com/UranusOJ/docs/edit/main/docs/:path',
+      text: '在 GitHub 上编辑此页'
+    },
+
+    // 最后更新时间
+    lastUpdated: true,
+    lastUpdatedText: '最后更新'
+  },
+  lastUpdated: true,
+  cleanUrls: true
 })
