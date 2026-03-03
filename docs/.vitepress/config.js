2ccdbc473723cc429dff374bc972e74e278321f8
imsunxinhao <imsunxinhao@163.com>
2026-03-03 09:34:45 +0800

    Update site description and sidebar in config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,15 @@
+import { defineConfigWithTheme } from 'vitepress'
 export default {
-  // 站点级选项
+  // 应用级配置选项
+  lang: 'zh-CN',
   title: 'UranusOJ 文档',
-  description: '一个 Online-Judge 系统，基于 Hydro。',
-
-  themeConfig: {
-    // 主题级选项
-  }
+  description: '使用 Hydro 构建的稳定，插件开源的 Online-Judge 系统。'
+  sidebar: [
+      {
+        text: '导航',
+        items: [
+          { text: '关于 UranusOJ', link: '/about-uranusoj' }
+        ]
+      }
+    ]
 }
